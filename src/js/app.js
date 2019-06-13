10f021f26c603d2b410433dd27ebe884f8625d27
ivankukarin <ikukarin@yandex.ru>
2019-06-14 01:08:56 +0400

    began to write test

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,22 @@
-let hero = {
-  name: 'Маг' , 
-  health: 90 
-  };
+const hero = {
+  name: 'Маг',
+  health: 90
+} ;
 
 const defineColorHealth = (person) => {
-    let healtMarker;
-    if (person.health > 50) { 
-      healtMarker = 'healthy';
-    } else if (person.health < 50)  { 
-      healtMarker = 'critical';
-    } else { 
-      healtMarker = 'critical';
-    }
-    return ${person.name}${healtMarker};
-}
+  let healtMarker;
+  if (person.health > 50) {
+    healtMarker = 'healthy';
+  } else if (person.health < 50)  { 
+    healtMarker = 'critical';
+  } else {
+    healtMarker = 'critical';
+  }
+  return `${person.name} ${healtMarker}`;
+};
 
-let colorHero = defineColorHealth(hero);
+const colorHero = defineColorHealth(hero);
+
 console.log(colorHero);
 
+export { hero, defineColorHealth, colorHero };
--- test/testapp.js
+++ test/testapp.js
@@ -1,1 +1,9 @@
-import {hero, defineColorHealth, colorHero} from './src/js/app.js' 
+import { hero, defineColorHealth, colorHero } from './src/js/app.js' 
+
+test('check the color of health', () => {
+  // Функция проверки
+  // 1. Выполняем нужные нам действия
+  TODO:
+  // 2. Проверяем результат с помощью:
+  expect(<что получили>).toBe(<что должно быть>);
+  })
